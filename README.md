f72ff9165504d087bb0451f4152da4e3d82b5b65
Norman <bluefish102615@gmail.com>
2020-11-11 15:27:54 +0800

    Updated Readme.md, changes to Quali page

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # ng-f1-stats
  F1 Stats using Angular
+
+
+API Endpoint: 
+
+https://ergast.com/mrd/
+
+Angular 10 Features
+
+  - Angular CLI
+    -----------
+	
+     Installation	
+	 $ npm i -g @angular/cli
+	 
+	 Create a new Angular App	 
+	 $ ng new ng-f1-stats
+	 
+	 Add new Component	 
+	 $ ng generate component components/[component-name]
+	 or
+	 $ ng g c components/[component-name]
+	 
+  - Routing
+    -------
+	 - https://angular.io/guide/router
+	 
+  - Using Angular Material CSS 
+    --------------------------
+    - https://material.angular.io/  
+	
+  - API call using Angular HTTP
+    ---------------------------
+     - https://rxjs.dev/api
+     ○ Promises
+	 ○ RxJs
+	  - Operators
+  - State Management
+     https://ngrx.io/
+     - NgRx
+	    ○ Store
+		○ Effects
+		○ Actions
--- f1-stats/src/app/effects/races.effects.ts
+++ f1-stats/src/app/effects/races.effects.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { EMPTY, Observable, of } from 'rxjs';
+import { EMPTY, of } from 'rxjs';
 import { map, mergeMap, catchError, concatMap, withLatestFrom, tap } from 'rxjs/operators';
-import { RacesLoaded, RacesLoadQualifyingStart, RacesLoadQualifyingSuccess } from '../actions/races.actions';
+import { RacesLoadQualifyingStart, RacesLoadQualifyingSuccess } from '../actions/races.actions';
 import { Race } from '../models/races.models';
 import { QualifyingResult } from '../models/results.models';
 import { F1Service } from "../services/f1-service";
@@ -11,27 +11,29 @@ import { F1Service } from "../services/f1-service";
 @Injectable()
 export class RacesEffects {
 
-    loadRaces$ = createEffect(() => this.actions$.pipe(
-        ofType('[RACES] Load Races'),
-        concatMap(action => of(action).pipe(
-            withLatestFrom(this.store.select(store => store.races)),
-        )),
-        mergeMap(([action, races]) => {
-            if (races.length > 0) {
-                console.log('Races Loaded Already');
-                return []; //to prevent error when reloading
-            } else {
-                return this.f1Service.getRaces()
-                    .pipe(
-                        map(races => {
-                            console.log("races", races);
-                            return { type: '[RACES] Load Success', payload: <Race[]>races }
-                        }),
-                        catchError(() => EMPTY)
-                    )
-            }
-        })
-    )
+    loadRaces$ = createEffect(() => this.actions$
+        .pipe(
+            ofType('[RACES] Load Races'),
+            concatMap(action => of(action)
+                .pipe(
+                    withLatestFrom(this.store.select(store => store.races)),
+                )),
+            mergeMap(([action, races]) => {
+                if (races.length > 0) {
+                    console.log('Races Loaded Already');
+                    return []; //to prevent error when reloading
+                } else {
+                    return this.f1Service.getRaces()
+                        .pipe(
+                            map(races => {
+                                console.log("races", races);
+                                return { type: '[RACES] Load Success', payload: <Race[]>races }
+                            }),
+                            catchError(() => EMPTY)
+                        )
+                }
+            })
+        )
     );
 
     loadQualifyingResults$ = createEffect(
